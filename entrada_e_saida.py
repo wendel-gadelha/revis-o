248bd9439f683198b237a3142e1035b7446af124
Wendel <wendel.warte@gmail.com>
2024-04-01 20:34:30 -0300

    Resolve questão 2

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -2,3 +2,11 @@ def usuario():
     nome = input("Qual o seu nome? ")
     print("Seja bem-vindo,", nome,"!")
 usuario()
+
+def somar():
+    n1 = int(input('Digite o primeiro número: '))
+    n2 = int(input('Digite o segundo número: '))
+    soma = n1 + n2
+    print('A soma dos dois números e: ', soma)
+
+somar()
