340cb09c69488b95ff2ea7f47b38384389ad580a
Wendel <wendel.warte@gmail.com>
2024-04-01 21:18:23 -0300

    Resolvida a questão 6

---
+++ condicionais.py
@@ -0,0 +1,7 @@
+def maior_de_idade():
+    idade = int(input('Informe sua idade: '))
+    if(idade >= 18):
+        print("Você é maior de idade!")
+    else:
+        print("Você é menor de idade.")
+maior_de_idade()
