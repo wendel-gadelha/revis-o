9ef51098483a3cf947f74ea6f24c66ace35d42fe
Wendel <wendel.warte@gmail.com>
2024-04-01 21:24:49 -0300

    Resolvida a questão 7

--- condicionais.py
+++ condicionais.py
@@ -5,3 +5,12 @@ def maior_de_idade():
     else:
         print("Você é menor de idade.")
 maior_de_idade()
+
+def par_ou_impar():
+    n1 = float(input('Digite um número: '))
+    resto = n1 % 2
+    if(resto == 0):
+        print('O número informado é par!')
+    else:
+        print('O número informado é ímpar')
+par_ou_impar()
